2ed845ba165be85164262e673ac46200d466e523
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2022-08-15 00:00:57 +0200

    Initial Commit

---
+++ HoopShop/HoopShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoopShopApp.swift
+//  HoopShop
+//
+//  Created by Dmytro Nimchynskyi on 15/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct HoopShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
