1f24f7e3e38909969f18210011510a8db2aab06b
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2022-08-17 00:52:34 +0200

    Added navigation bar in detailed product view

---
+++ HoopShop/View/Detail/NavigationBarDetailView.swift
@@ -0,0 +1,37 @@
+//
+//  NavigationBarDetailView.swift
+//  HoopShop
+//
+//  Created by Dmytro Nimchynskyi on 17/08/2022.
+//
+
+import SwiftUI
+
+struct NavigationBarDetailView: View {
+    var body: some View {
+        HStack {
+            Button(action: {}, label: {
+                Image(systemName: "chevron.left")
+                    .font(.title)
+                    .foregroundColor(.white)
+            })
+            
+            Spacer()
+            
+            Button(action: {}, label: {
+                Image(systemName: "cart")
+                    .font(.title)
+                    .foregroundColor(.white)
+            })
+        }//: HStack
+    }
+}
+
+struct NavigationBarDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBarDetailView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(Color.gray)
+    }
+}
