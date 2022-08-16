2960be58eb6f69ef76f0d000add8f565b4ee950d
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2022-08-16 22:07:45 +0200

    Fixed featured items display

--- HoopShop/View/Home/FeaturedItemView.swift
+++ HoopShop/View/Home/FeaturedItemView.swift
@@ -6,10 +6,13 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseStorage
 
 struct FeaturedItemView: View {
     // MARK: - PROPERTY
     let featuredProduct: FeaturedProduct
+//    let storageReference = storage.reference("images/hoop.jpeg")
     
     // MARK: - BODY
     var body: some View {
@@ -23,7 +26,7 @@ struct FeaturedItemView: View {
 // MARK: - PREVIEW
 struct FeaturedItemView_Previews: PreviewProvider {
     static var previews: some View {
-        FeaturedItemView(featuredProduct: featuredProducts[0])
+        FeaturedItemView(featuredProduct: featuredProducts[5])
             .previewLayout(.sizeThatFits)
             .padding()
             .background(colorBackground)
