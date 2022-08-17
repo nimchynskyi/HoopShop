7948c8c7d5505da015ed180a7639a9fb66025abe
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2022-08-17 23:47:01 +0200

    Fixed to display selected product info

--- HoopShop/View/Detail/HeaderDetailView.swift
+++ HoopShop/View/Detail/HeaderDetailView.swift
@@ -8,11 +8,15 @@
 import SwiftUI
 
 struct HeaderDetailView: View {
+    // MARK: - Property
+    @EnvironmentObject var shop: Shop
+    
+    // MARK: - Body
     var body: some View {
         VStack(alignment: .leading, spacing: 6, content: {
             Text("Basketball shoes")
             
-            Text(sampleProduct.name)
+            Text(shop.selectedProduct?.name ?? sampleProduct.name)
                 .font(.largeTitle)
                 .fontWeight(.black)
         })//: VStack
@@ -20,9 +24,11 @@ struct HeaderDetailView: View {
     }
 }
 
+// MARK: - Preview
 struct HeaderDetailView_Previews: PreviewProvider {
     static var previews: some View {
         HeaderDetailView()
+            .environmentObject(Shop())
             .previewLayout(.sizeThatFits)
             .padding()
             .background(Color.gray)
--- HoopShop/View/Detail/TopPartDetailView.swift
+++ HoopShop/View/Detail/TopPartDetailView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct TopPartDetailView: View {
     // MARK: - PROPERTY
+    @EnvironmentObject var shop: Shop
     @State private var isAnimating: Bool = false
     
     // MARK: - BODY
@@ -19,7 +20,7 @@ struct TopPartDetailView: View {
                 Text("Price")
                     .fontWeight(.semibold)
                 
-                Text(sampleProduct.formattedPrice)
+                Text(shop.selectedProduct?.formattedPrice ?? sampleProduct.formattedPrice)
                     .font(.largeTitle)
                     .fontWeight(.black)
                     .scaleEffect(1.2, anchor: .leading)
@@ -29,7 +30,7 @@ struct TopPartDetailView: View {
             Spacer()
             
             // Photo
-            Image(sampleProduct.image)
+            Image(shop.selectedProduct?.image ?? sampleProduct.image)
                 .resizable()
                 .scaledToFit()
                 .offset(y: isAnimating ? 0 : -35)
@@ -47,6 +48,7 @@ struct TopPartDetailView: View {
 struct TopPartDetailView_Previews: PreviewProvider {
     static var previews: some View {
         TopPartDetailView()
+            .environmentObject(Shop())
             .previewLayout(.sizeThatFits)
             .padding()
     }
