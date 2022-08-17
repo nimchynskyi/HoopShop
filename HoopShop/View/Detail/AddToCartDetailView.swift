7ca64736888a81075a42824a304ec58c5f708d97
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2022-08-17 23:46:08 +0200

    Added add to cart button

---
+++ HoopShop/View/Detail/AddToCartDetailView.swift
@@ -0,0 +1,46 @@
+//
+//  AddToCartDetailView.swift
+//  HoopShop
+//
+//  Created by Dmytro Nimchynskyi on 17/08/2022.
+//
+
+import SwiftUI
+
+struct AddToCartDetailView: View {
+    // MARK: - Property
+    @EnvironmentObject var shop: Shop
+    
+    // MARK: - Body
+    var body: some View {
+        Button(action: {
+            feedback.impactOccurred()
+        }, label: {
+            Spacer()
+            Text("Add to cart".uppercased())
+                .font(.system(.title2, design: .rounded))
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+            Spacer()
+        })
+        .padding(15)
+        .background(
+            Color(
+                red: shop.selectedProduct?.red ?? sampleProduct.red,
+                green: shop.selectedProduct?.green ?? sampleProduct.green,
+                blue: shop.selectedProduct?.blue ?? sampleProduct.blue
+            )
+        )
+        .clipShape(Capsule())
+    }
+}
+
+// MARK: - Preview
+struct AddToCartDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddToCartDetailView()
+            .environmentObject(Shop())
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
