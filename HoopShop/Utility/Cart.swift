5d8707b8c4924686da64150df51175ea20367df4
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2022-08-21 22:14:44 +0200

    Added additional functionality to Cart model

--- HoopShop/Utility/Cart.swift
+++ HoopShop/Utility/Cart.swift
@@ -8,22 +8,62 @@
 import Foundation
 
 struct CartProduct: Identifiable {
-    let id: Int
-    let name: String
-    let image: String
-    let price: Int
     
-    var formattedPrice: String {
-        if price == 0 {
-            return ""
+    var id: String
+    var product: Product
+    var count: Int
+    
+    init(product: Product, count: Int) {
+        self.id = UUID().uuidString
+        self.product = product
+        self.count = count
+    }
+}
+
+// To compare CartProduct for removal
+extension CartProduct : Equatable {
+    static func == (lhs: CartProduct, rhs: CartProduct) -> Bool {
+        if lhs.id == rhs.id {
+            return true
         } else {
-            return "$\(price)"
+            return false
         }
     }
 }
 
 class Cart: ObservableObject {
-    @Published var test: String = "EnvTest"
+    @Published var cartItems: [CartProduct] = []
     @Published var showingCart: Bool = false
-    @Published var cartItems: [Product] = []
+    
+    var count = 0
+    var countTotal = 0
+    var subtotal = 0.0
+    var addNewProduct = true
+    
+    func addProduct(product: Product){
+        let price = Double(product.price)
+        subtotal += price
+        subtotal *= Double(count)
+        
+        var addNewProduct = true
+        for (index, item) in cartItems.enumerated() {
+            if item.product.id == product.id {
+                cartItems[index].count = cartItems[index].count + self.count
+                self.countTotal += self.count
+                addNewProduct = false
+            }
+        }
+        if addNewProduct {
+            cartItems.append(CartProduct(product: product, count: self.count))
+            self.countTotal += self.count
+        }
+        
+        count = 0
+    }
+    
+    func removeProduct(product: CartProduct) {
+        self.countTotal -= product.count
+        subtotal -= Double(product.product.price * product.count)
+        cartItems.removeAll(where: { $0 == product })
+    }
 }
