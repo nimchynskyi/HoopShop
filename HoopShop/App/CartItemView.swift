1b4a77e5b1a391533c310a5ace73ecf3efa42655
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2022-08-21 22:11:36 +0200

    Added view for separate cart items

--- HoopShop/App/CartItemView.swift
+++ HoopShop/App/CartItemView.swift
@@ -8,13 +8,35 @@
 import SwiftUI
 
 struct CartItemView: View {
+    @EnvironmentObject var cart: Cart
+    var cartItem: CartProduct
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(cartItem.product.image)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 100, height: 100)
+                .clipShape(Circle())
+            
+            VStack(alignment: .leading) {
+                Text(cartItem.product.name)
+                    .fontWeight(.semibold)
+                
+                Text("$\(cartItem.product.price)")
+                
+                Button("Show details"){}
+                    .foregroundColor(.gray)
+            }
+            Spacer()
+            Text("x \(cartItem.count)")
+        }
     }
 }
 
+
 struct CartItemView_Previews: PreviewProvider {
     static var previews: some View {
-        CartItemView()
+        CartItemView(cartItem: sampleOrder)
     }
 }
