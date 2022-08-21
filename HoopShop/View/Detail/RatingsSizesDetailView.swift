f97c36b13a690350eb473aa5fee1f6ee05ed60b3
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2022-08-22 01:08:24 +0200

    Added separate sizes for different typeProducts

--- HoopShop/View/Detail/RatingsSizesDetailView.swift
+++ HoopShop/View/Detail/RatingsSizesDetailView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct RatingsSizesDetailView: View {
     // MARK: - PROPERTY
-    let sizes: [String] = ["XS", "S", "M", "L", "XL"]
+    @EnvironmentObject var shop: Shop
+    @State var ballSizes: [String] = ["3", "4", "5", "6", "7"]
+    @State var shoesSizes: [String] = ["42", "43", "44", "45", "46"]
+    
     
     // MARK: - BODY
     var body: some View {
@@ -42,19 +45,36 @@ struct RatingsSizesDetailView: View {
                     .foregroundColor(colorGray)
                 
                 HStack(alignment: .center, spacing: 5, content: {
-                    ForEach(sizes, id: \.self) { size in
-                        Button(action: {}, label: {
-                            Text(size)
-                                .font(.footnote)
-                                .fontWeight(.heavy)
-                                .foregroundColor(colorGray)
-                                .frame(width: 28, height: 28, alignment: .center)
-                                .background(Color.white.cornerRadius(5))
-                                .background(
-                                    RoundedRectangle(cornerRadius: 5)
-                                        .stroke(colorGray, lineWidth: 2)
-                                )
-                        })
+                    if shop.selectedProduct?.productType == "ball" {
+                        ForEach(ballSizes, id: \.self) { size in
+                            Button(action: {}, label: {
+                                Text(size)
+                                    .font(.footnote)
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(colorGray)
+                                    .frame(width: 28, height: 28, alignment: .center)
+                                    .background(Color.white.cornerRadius(5))
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 5)
+                                            .stroke(colorGray, lineWidth: 2)
+                                    )
+                            })
+                        }
+                    } else {
+                        ForEach(shoesSizes, id: \.self) { size in
+                            Button(action: {}, label: {
+                                Text(size)
+                                    .font(.footnote)
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(colorGray)
+                                    .frame(width: 28, height: 28, alignment: .center)
+                                    .background(Color.white.cornerRadius(5))
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 5)
+                                            .stroke(colorGray, lineWidth: 2)
+                                    )
+                            })
+                        }
                     }
                 })//: HStack
             })//: VStack
@@ -66,6 +86,7 @@ struct RatingsSizesDetailView: View {
 struct RatingsSizesDetailView_Previews: PreviewProvider {
     static var previews: some View {
         RatingsSizesDetailView()
+            .environmentObject(Shop())
             .previewLayout(.sizeThatFits)
             .padding()
     }
