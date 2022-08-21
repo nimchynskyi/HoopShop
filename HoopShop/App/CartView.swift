478d22ce2add727ff5434c35a6eea71b13c7c6e3
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2022-08-21 22:10:38 +0200

    Added NavigationView to display Cart items

--- HoopShop/App/CartView.swift
+++ HoopShop/App/CartView.swift
@@ -8,18 +8,42 @@
 import SwiftUI
 
 struct CartView: View {
+    @EnvironmentObject var shop: Shop
     @EnvironmentObject var cart: Cart
+    @EnvironmentObject var firestore: FirestoreManager
+    @State private var isAnimated: Bool = false
     
     var body: some View {
-        VStack{
-            Text(cart.test)
+        NavigationView {
+            List (cart.cartItems) { cartItem in
+                NavigationLink (destination: CartItemDetail(cartItem: cartItem)){
+                    CartItemView(cartItem: cartItem)
+                }.swipeActions {
+                    Button(action: {
+                        withAnimation{
+                            cart.removeProduct(product: cartItem)
+                        }
+                    }, label: {
+                        Text("Delete")
+                    })
+                    .tint(.red)
+                }
+            }
+            .navigationTitle("My Cart")
         }
+        .navigationViewStyle(StackNavigationViewStyle())
+        
+        CartSummaryView()
+        
+        CartPaymentView()
     }
 }
 
 struct CartView_Previews: PreviewProvider {
     static var previews: some View {
         CartView()
+            .environmentObject(Shop())
             .environmentObject(Cart())
+            .environmentObject(FirestoreManager())
     }
 }
